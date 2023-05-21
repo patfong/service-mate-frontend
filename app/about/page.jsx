12974a34b245bae90c35a56659c3657cecee5f8c
patfong <patfong82@gmail.com>
2023-05-21 15:50:50 +1000

    Initial commit

---
+++ app/about/page.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <h2 className="text-lg py-2 m-4">This is the About Us Page</h2>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["var(--font-montserrat)"]
+      },
+      gridTemplateColumns : {
+        fluid: "repeat(auto-fit,minmax(15rem,1fr))",
+      },
+    },
+  },
+  plugins: [],
+}
